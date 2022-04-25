a04d8355d19481935020f9f24b809da762823940
Javier Garcia <javii_93_27@hotmail.com>
2022-04-24 23:58:53 -0300

    operadores ternarios

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -9,7 +9,17 @@ export default function Hooks() {
     };
     return (
         <div>
-            <h1>Contamos {cuenta} obejitas</h1>
+            {/* operador ternario  */}
+            {cuenta === 0 ?(<h1>Arranquemos a contar a ver si nos dormimos.</h1>):(<h1>
+            
+            {cuenta>1 ? (<h1>Contamos {cuenta} obejitas</h1>) : 
+            (<h1>Contamos {cuenta} obejita</h1>)}
+            {cuenta && <h4>Que bueno a ver si nos dormimos</h4>}
+
+            </h1>)}
+            {/* operador ternario */}
+
+            {/* <h1>Contamos {cuenta} obejitas</h1> //con ternario se va arriba */}
             <button onClick={handleClick}>Contar otra</button>
         </div>
     );
